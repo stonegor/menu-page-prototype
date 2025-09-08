b65e199da72a57a15e2288fdf9bda5cb37c74f5f
geoheat.ed_vkusvill.ru <sstonegor@gmail.com>
2025-09-08 19:10:05 +0300

    asdklas

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,6 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/menu-prototypu' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/menu-prototypu/' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
